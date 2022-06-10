0d785bd46fe93d745010c00d27b491286e1426cc
adeelsuviyanto <adeelsuviyanto@gmail.com>
2022-06-10 09:58:52 +0700

    Troubleshooting on authorization from authentication token

--- src/index.js
+++ src/index.js
@@ -76,8 +76,11 @@ web.use(async (req, res, next) => {
 
 //Firebase Authentication initialization
 function checkAuth(req, res, next) {
-  if(req.headers.authtoken){
-    admin.auth().verifyIdToken(req.headers.authtoken).then(next()).catch(res.status(403).send('Unauthorized'));
+  let auth = req.header['Authorization'];
+  if(auth){
+    let bearer = auth.split(' ');
+    console.log(bearer[2]);
+    admin.auth().verifyIdToken(bearer[2]).then(next()).catch(res.status(403).send('Unauthorized'));
   }
   else res.status(403).send('Unauthorized');
 }
