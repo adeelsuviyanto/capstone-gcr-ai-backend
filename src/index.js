195ac48c75767e8fe631f8afdb2a72a954840073
adeelsuviyanto <adeelsuviyanto@gmail.com>
2022-06-12 15:38:53 +0700

    Troubleshooting file uploads

--- src/index.js
+++ src/index.js
@@ -227,12 +227,7 @@ web.post('/predict', upload.single('file'), (req, res, next) => {
     res.status(400).send('No patient ID provideed.').end();
   }
   //Piping to ML backend
-  const newurl = 'https://getpredict-d34xsyfyta-as.a.run.app'
-  req.pipe(request({
-    qs: req.query,
-    uri: newurl
-  })).pipe(res);
-  
+  const newurl = 'https://getpredict-d34xsyfyta-as.a.run.app';
   //Storing file to cloud storage
   const fileName = 'PRED' + '-' + req.query.patientid + '-' + Date.now() + '.' + req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
   const blob = bucket.file(fileName);
@@ -243,7 +238,7 @@ web.post('/predict', upload.single('file'), (req, res, next) => {
   });
   blobStream.on('finish', () => {
     const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
-    //res.status(200).send(publicUrl + ' ' + 'Upload Success.');
+    res.status(200).send(publicUrl + ' ' + 'Upload Success.');
   });
 
   blobStream.end(req.file.buffer);
